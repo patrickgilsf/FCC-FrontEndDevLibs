7a9b1fcf783d525f1c04efb4c6a4f6b7563c9d6e
patrickgilsf <johnpatrickgilligan@gmail.com>
2022-08-09 08:44:12 -0700

    adding my work from redux on Macbook Air

---
+++ FrontEndDevLibs-Redux.jsx
@@ -0,0 +1,19 @@
+//Create a Redux Store
+const reducer = (state = 5) => {
+    return state;
+  }
+  
+  // Redux methods are available from a Redux object
+  // For example: Redux.createStore()
+  // Define the store here:
+  const store = Redux.createStore(reducer);
+
+  //Get State from the Redux Store
+  const store = Redux.createStore(
+    (state = 5) => state
+  );
+  
+  // Change code below this line
+  const currentState = store.getState();
+
+  
