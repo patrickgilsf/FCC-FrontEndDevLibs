e6c27f0835d71c54b57c60c05b77ff16fd57d902
patrickgilsf <johnpatrickgilligan@gmail.com>
2022-08-09 08:46:38 -0700

    didnt save redux work, file was incomplete

--- FrontEndDevLibs-Redux.jsx
+++ FrontEndDevLibs-Redux.jsx
@@ -3,17 +3,45 @@ const reducer = (state = 5) => {
     return state;
   }
   
-  // Redux methods are available from a Redux object
-  // For example: Redux.createStore()
-  // Define the store here:
-  const store = Redux.createStore(reducer);
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+const store = Redux.createStore(reducer);
 
-  //Get State from the Redux Store
-  const store = Redux.createStore(
-    (state = 5) => state
+//Get State from the Redux Store
+const store = Redux.createStore(
+(state = 5) => state
+);
+// Change code below this line
+const currentState = store.getState();
+
+//Define a Redux Action
+
+const action = {
+    type: 'LOGIN'
+}
+
+//Define an Action Creator
+const action = {
+    type: 'LOGIN'
+  }
+// Define an action creator here:
+const actionCreator = () => {
+return action
+}
+
+//Dispatch an Action Event
+const store = Redux.createStore(
+    (state = {login: false}) => state
   );
   
-  // Change code below this line
-  const currentState = store.getState();
+const loginAction = () => {
+return {
+    type: 'LOGIN'
+}
+};
 
-  
+// Dispatch the action here:
+store.dispatch(loginAction());
+
+//Handle an Action in the Store
